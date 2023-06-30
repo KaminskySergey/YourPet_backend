d1e3dbd8a98f466138db985b6370b0977fba3650
KaminskySergey <kaminsky_s_s@ukr.net>
2023-06-30 15:28:37 +0300

    Update allPetsById.js

--- controllers/pet/allPetsById.js
+++ controllers/pet/allPetsById.js
@@ -4,11 +4,11 @@ const { Pet } = require("../../models/Pet");
 const allPetsById = async (req, res, next) => {
 const {_id} = req.user;
 
-const allPets = await Pet.find({user: _id})
+const result = await Pet.find({user: _id})
     res.json({
         status: "success",
         code: 200,
-        allPets,
+        result,
       });
 }
 
